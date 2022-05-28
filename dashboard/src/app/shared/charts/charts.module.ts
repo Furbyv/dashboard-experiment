02cef0027e47e8a8af4934f02d7b83f228c63ef8
Furbyv <fbverbeek@gmail.com>
2022-05-28 19:33:52 +0200

    Add variable pie chart

--- dashboard/src/app/shared/charts/charts.module.ts
+++ dashboard/src/app/shared/charts/charts.module.ts
@@ -5,12 +5,15 @@ import { ChartModule } from 'angular-highcharts';
 import { AreaChartComponent } from './area-chart/area-chart.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import highchartsMore from 'highcharts/highcharts-more';
+import variablePie from 'highcharts/modules/variable-pie';
 import * as Highcharts from 'highcharts';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 highchartsMore(Highcharts);
+variablePie(Highcharts);
 
 @NgModule({
-  declarations: [LineChartComponent, AreaChartComponent],
+  declarations: [LineChartComponent, AreaChartComponent, PieChartComponent],
   imports: [CommonModule, ChartModule, FormsModule],
-  exports: [LineChartComponent, AreaChartComponent],
+  exports: [LineChartComponent, AreaChartComponent, PieChartComponent],
 })
 export class ChartsModule {}
