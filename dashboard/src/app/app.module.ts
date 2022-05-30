f7fd72f07fab0bef10df43ffaa6d925baeb0fc81
Furbyv <fbverbeek@gmail.com>
2022-05-30 08:14:11 +0200

    remove ktd grid

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { KtdGridModule } from '@katoid/angular-grid-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { GridsterGridComponent } from './gridster/gridster.component';
 import { GridsterModule } from 'angular-gridster2';
-import { KtdComponent } from './ktd-grid/ktd-grid.component';
 import { GridsterOptionsDirective } from './gridster/gridster-options/gridster-options.directive';
 import { GridActionBarComponent } from './gridster/grid-action-bar/grid-action-bar.component';
 import { CardTemplateComponent } from './gridster/card-template/card-template.component';
@@ -19,7 +17,6 @@ import { EditCellOverlayComponent } from './gridster/edit-cell-overlay/edit-cell
   declarations: [
     AppComponent,
     GridsterGridComponent,
-    KtdComponent,
     GridsterOptionsDirective,
     GridActionBarComponent,
     CardTemplateComponent,
@@ -29,7 +26,6 @@ import { EditCellOverlayComponent } from './gridster/edit-cell-overlay/edit-cell
   imports: [
     BrowserModule,
     AppRoutingModule,
-    KtdGridModule,
     BrowserAnimationsModule,
     MaterialModule,
     GridsterModule,
